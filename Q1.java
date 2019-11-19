2d7763afc7e927693346798b97ca84c34dc7bea0
OliverFlexa <54111538+OliverFlexa@users.noreply.github.com>
2019-11-19 18:25:50 -0300

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Q1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double nota[] = new double[3];
+        double mediar;
+        int x;
+
+        double mediap;
+        double mediapi[] = new double[2];
+
+        x = 0;
+
+        for(int i=0; i<vetor.length; i++){
+            
+        }
+        for(int i=0; i<vetor.length; i++){
+            System.out.println("Digite a "+i+"° Nota."); 
+            nota[i] = sc.nextDouble();
+        }
+        
+
+        for(int i=0; i<vetor.length; i++){
+            x = nota[i] + x;
+            mediar = x;  
+        }
+        for(int i=0; i<vetor.length; i++){
+            mediaapi[i] = nota[i];
+        }
+        mediar = media/3;
+        if(nota[3] = A){
+            System.out.println("Sua média é de " + mediar+ ".");
+        }
+        else if(nota[3] = P){
+            System.out.println("Sua média ponderada é de " + mediap);
+            mediap = 
+
+
+
+        }
+        
+
+
+
+
+
+
+    }
+}
